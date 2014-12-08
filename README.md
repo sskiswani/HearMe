f7c0756943328f9c7032950c5f44cfd4d4672ffe
Sam Kiswani <ssk09c@my.fsu.edu>
2014-12-08 07:10:50 -0500

    detecting staff lines using an obtuse method that probably doesnt work :o

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 HearMe
 ======
 
-Sheet Music --> MIDI converter
+Parse sheet music into a playable MIDI file. The proper phrasing is _Optical Music Recognition_.
+
+To use, simply run `hearme.py` with the path to an image of sheet music supplied as an argument, e.g. `> python hearme.py source.png`.
+
+Prerequisites
+-------------
+
+- [scikit-image](http://scikit-image.org/) for image analysis
+- [music21](http://web.mit.edu/music21/) for MIDI creation
+- [numpy](http://www.numpy.org/) misc.
+- scipy
+- matplotlib 
--- hearme.py
+++ hearme.py
@@ -1,15 +1,56 @@
 import argparse
+from os import path
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage import (filter, io, feature, img_as_float)
+from scipy.ndimage.filters import convolve1d
 
+MASKS = {
+    'sobel': np.array([-1., 0., 1.]) * 0.5,
+    'gauss': np.array([1., 4., 6., 4., 1.]) / 16.
+}
+
+def histogram(img, thresh=0.9):
+    hist = np.zeros((img.shape[0],))
+
+    for y in xrange(1, img.shape[0]-1):
+        for x in xrange(1, img.shape[1]-1):
+            if img[y, x] < thresh and img[y, x-1] < thresh and img[y, x+1] < thresh:
+                hist[y] += 1
+
+    return hist
+
+def normalize(img):
+    imin, imax = np.min(img), np.max(img)
+    if imin == imax: imax = imin + 1E-3
+    return (img - imin) / (imax - imin)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='HearMe: Sheet Music to MIDI converter.')
-    parser.add_argument('src', '--s', type=str, help='path to source.')
-    parser.add_argument('dest', '--d', type=str, nargs='?', help='name to save midi as.')
-    parser.set_defaults(dest=None)
+    parser.add_argument('src', type=str, nargs='?', help='Path to source image (sheet music) to be parsed.')
+    parser.add_argument('dest', type=str, nargs='?', help='Name to save midi as, if not supplied will save output in same directory as source.')
+    parser.set_defaults(dest=None, src='../BoleroofFire.jpg')
     args = parser.parse_args()
 
-    print 'hi'
+    # Initialize
+    src = path.abspath(args.src)
+    if args.dest is None:
+        dest = path.basename(src)
+        dest = path.abspath(dest.replace(dest.split('.')[-1], 'midi'))
+    else:
+        dest = path.abspath(args.dest)
 
+    img = img_as_float(io.imread(src, True))
+    img = convolve1d(filter.vsobel(img), np.array([1., 2., 2., 6., 2., 2., 1.]) / 16., 1, mode='nearest')
+    img = 1 - img_as_float(io.imread(src, True)) - normalize(img)
+    print 'min: %.3f max: %.3f' % (np.min(img), np.max(img))
+    io.imshow(img)
+    hist = np.array([np.sum(img[y, :] > 0.4) for y in xrange(img.shape[0])])
+    plt.scatter(hist, range(img.shape[0]), c='r')
+    avg = 2*np.average(hist)
+    lines = np.array([hist[y] - hist[y-1] > avg for y in xrange(len(hist))]) * img.shape[0] * 0.75
 
+    plt.scatter(lines, range(img.shape[0]), c='b')
+    plt.show()
 
     exit(0)
